2fd4825537545b49334d1572bf28250f84ae4c8b
amuthap <53494696+amuthap@users.noreply.github.com>
2025-08-07 00:16:34 +0530

    Update birthday_script.py

--- birthday_script.py
+++ birthday_script.py
@@ -191,11 +191,11 @@ def main():
                 #else:
                 #    print(f"✅ Uploaded to Picnie: {out_url}")
                    # whatsapp_number = (row.get("WhatsApp") or row.get("Phone") or "").strip()
-                    whatsapp_number = row.get("WhatsApp")
-                    if whatsapp_number:
-                        send_whatsapp_message(whatsapp_number, out_path, name)
-                    else:
-                        print(f"⚠️ No WhatsApp number for {name}")
+                whatsapp_number = row.get("WhatsApp")
+                if whatsapp_number:
+                    send_whatsapp_message(whatsapp_number, out_path, name)
+                else:
+                    print(f"⚠️ No WhatsApp number for {name}")
             except Exception as e:
                 print(f"❌ Failed to save image for {name}: {e}")
 
